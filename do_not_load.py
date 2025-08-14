fcc9be0869a0e18922081b745b4d2fb643018f84
Nick Raymond <nickraymond99@gmail.com>
2025-08-14 09:06:07 -0700

    Remove do_not_load.py from repo and ignore it

--- do_not_load.py
+++
@@ -1,5 +0,0 @@
-# Specify Spotter ID and API Token
-##spotterId = "SPOT-32010C" # BM camera module
-
-#token = "019058d08091ed2b1f96e774344874" # NR Admin
-#token = "90bda8b0a39b9570b548ab6f7a1aea" Spotter ENG
